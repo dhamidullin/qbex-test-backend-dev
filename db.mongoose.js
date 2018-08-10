57d15a584a9fd41b4688aebcbff330f89a6b5bb0
dhamidullin <danis.zp.123@gmail.com>
2018-08-10 15:22:00 +0300

    db

--- db.mongoose.js
+++ db.mongoose.js
@@ -26,8 +26,21 @@ mongoose.connect('mongodb://localhost/qbex-test-backend-dev', (err, db) => {
             default: 'user'
         }
     });
+    const ItemSchema = new Schema({
+        title: {
+            type: String,
+            unique: true
+        },
+        images: {
+            type: Array
+        },
+        description: {
+            type: String
+        }
+    });
 
     const UserModel = mongoose.model('UserModel', UserSchema, "users");
+    const ItemModel = mongoose.model('ItemModel', ItemSchema, "items");
 
     exports.findUserByUsername = (username, callback) => {
         UserModel.findOne({ username: username }, (err, doc) => {
@@ -43,6 +56,43 @@ mongoose.connect('mongodb://localhost/qbex-test-backend-dev', (err, db) => {
         });
     }
 
+    exports.findItemById = (id, callback) => {
+        ItemModel.findOne({ _id: ObjectId(id) }, (err, doc) => {
+            callback(err, doc);
+        });
+    }
+
+    exports.addItem = (item, callback) => {
+        var newItem = new ItemModel(item);
+        newItem.save((err) => {
+            callback(err);
+        });
+    }
+
+    var item = {
+        id: 0,
+        title: 'NVIDIA GTX 1060',
+        images: [
+            'https://i.ebayimg.com/images/g/HEAAAOSwkjta-tCV/s-l640.jpg',
+            'https://cdn2.riastatic.com/photosnew/general/adv_photos/hardware-vydeokarta-msi-geforce-gtx-1060-armor-6g-ocv1-912-v328-028__67289639m.jpg',
+            'https://cdn2.riastatic.com/photosnew/general/adv_photos/hardware-vydeokarta-inno3d-gtx-1060-6gb-gddr5-x2-n106f-2sdn-n5gs__85193804m.jpg'
+        ],
+        short_description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+        description: `<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+        
+        <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+    
+        <p>Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>
+    
+        <p>Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a,</p>`
+    };
+
+    var newItem = new ItemModel(item);
+    newItem.save((err) => {
+        console.log(err);
+    });
+
+
     // var admin = new UserModel({
     //     username: 'admin',
     //     password: 'admin',
@@ -51,4 +101,5 @@ mongoose.connect('mongodb://localhost/qbex-test-backend-dev', (err, db) => {
     // admin.save((err) => {
     //     console.log(err);
     // });
+
 });
--- index.routes.js
+++ index.routes.js
@@ -16,33 +16,38 @@ module.exports = function (app, db) {
         });
     });
 
-    // app.post('/sign-in', (req, res, next) => {
-    //     if (req.isAuthenticated())
-    //         return res.end(JSON.stringify({ err: "Вы уже выполнили вход." }));
-    //     passport.authenticate('localStrategy', (err, user) => {
-    //         console.log("err");
-    //         console.log(err);
-    //         console.log("user");
-    //         console.log(user);
+    app.post('/sign-in', (req, res, next) => {
+        if (req.isAuthenticated())
+            return res.end(JSON.stringify({ err: "Вы уже выполнили вход." }));
+        passport.authenticate('localStrategy', (err, user) => {
+            console.log("err");
+            console.log(err);
+            console.log("user");
+            console.log(user);
 
-    //         if (err)
-    //             return res.end(JSON.stringify({ err: err }));
+            if (err)
+                return res.end(JSON.stringify({ err: err }));
 
-    //         // этот метод необходим при использовании кастомной callback функции 
-    //         req.logIn(user, (err) => {
-    //             if (err)
-    //                 return res.send(JSON.stringify({ err: 'Error while login, please, try later' }));
-    //             else
-    //                 return res.send(JSON.stringify({ err: false }));
-    //         });
+            // этот метод необходим при использовании кастомной callback функции 
+            req.logIn(user, (err) => {
+                if (err)
+                    return res.send(JSON.stringify({ err: 'Error while login, please, try later' }));
+                else
+                    return res.send(JSON.stringify({ err: false }));
+            });
 
-    //         return res.end(JSON.stringify({ err: false }));
-    //     })(req, res, next);
-    // });
-    
+            return res.end(JSON.stringify({ err: false }));
+        })(req, res, next);
+    });
+
     app.get("/isAuthenticated", (req, res, next) => {
         res.end(JSON.stringify({ isAuthenticated: req.isAuthenticated() }));
     });
-
-
+    
+    app.get("/userObject", (req, res, next) => {
+        res.end(JSON.stringify({
+            user: req.user,
+            session: req.session
+        }));
+    });
 };
