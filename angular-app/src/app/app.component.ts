6d5b8865b15e0ca699b018adfc76aa59e7c5f2c0
dhamidullin <danis.zp.123@gmail.com>
2018-08-10 07:10:08 +0300

    tinymce

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'qbex';
+  data: String = '';
+
+  func() {
+    console.log(this.data);
+  }
+
 }
--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -5,32 +5,29 @@ import { HttpModule } from '@angular/http';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { AboutComponent } from './components/about/about.component';
+import { BasketComponent } from './components/basket/basket.component';
+import { CatalogComponent } from './components/catalog/catalog.component';
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ProductComponent } from './components/product/product.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 import { OnlyAdminGuard } from './guards/only-admin.guard';
-import { CatalogComponent } from './components/catalog/catalog.component';
-import { ProductComponent } from './components/product/product.component';
-import { AdminComponent } from './components/admin/admin.component';
-import { SignInComponent } from './components/sign-in/sign-in.component';
-import { SignUpComponent } from './components/sign-up/sign-up.component'
+
+import { EditorModule } from '@tinymce/tinymce-angular';
 
 const appRoutes: Routes = [
-  {
-    path: '', redirectTo: 'home', pathMatch: 'full'
-  },
-  {
-    path: 'home', component: HomeComponent
-  },
-  {
-    path: 'sign-in', component: SignInComponent
-  },
-  {
-    path: 'sign-up', component: SignUpComponent
-  },
-  {
-    path: '**', component: NotFoundComponent
-  }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'catalog', component: CatalogComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'admin', component: AdminComponent },
 ];
 
 @NgModule({
@@ -42,13 +39,16 @@ const appRoutes: Routes = [
     ProductComponent,
     AdminComponent,
     SignInComponent,
-    SignUpComponent
+    SignUpComponent,
+    AboutComponent,
+    BasketComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
     FormsModule,
-    HttpModule
+    HttpModule,
+    EditorModule
   ],
   providers: [OnlyAdminGuard],
   bootstrap: [AppComponent]
